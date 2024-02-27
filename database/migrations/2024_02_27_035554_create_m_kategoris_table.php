42eb482d245f9f64ec4164d8aab4890b6c3b6c5b
Yunila Putmasari <nilaputma26@gmail.com>
2024-02-27 11:18:35 +0700

    praktikum 2.1 migrasi level dan kategori

---
+++ database/migrations/2024_02_27_035554_create_m_kategoris_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_kategoris', function (Blueprint $table) {
+            $table->id('kategori_id');
+            $table->String('kategori_kode', 10);
+            $table->String('kategori_nama', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_kategoris');
+    }
+};
