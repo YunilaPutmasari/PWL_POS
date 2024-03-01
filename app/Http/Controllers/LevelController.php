7b883e0df158a9a4e648e0933eb42e8e6cdaebd5
Yunila Putmasari <nilaputma26@gmail.com>
2024-03-01 16:49:34 +0700

    Implementasi Eloquent ORM

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -11,8 +11,8 @@ class LevelController extends Controller
     public function index()
     {
 
-        // DB::insert('insert into m_levels(level_kode,level_nama,created_at) values(?,?,?)', ['CUS', 'pelanggan', now()]);
-        // return 'insert data baru berhasil';
+        DB::insert('insert into m_levels(level_kode,level_nama,created_at) values(?,?,?)', ['CUS', 'pelanggan', now()]);
+        return 'insert data baru berhasil';
 
         // $row = DB::update('update m_levels set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
         // return 'Update data berhasil. Jumlah data yang di update: ' . $row . 'baris';
@@ -20,8 +20,8 @@ class LevelController extends Controller
         // $row = DB::delete('delete from m_levels where level_kode = ?', ['CUS']);
         // return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row . 'baris';
 
-        $data = DB::Select('SELECT * FROM m_levels');
-        return view('level', ['data' => $data]);
+        // $data = DB::Select('SELECT * FROM m_levels');
+        // return view('level', ['data' => $data]);
 
 
     }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,6 @@ Route::get('level', [LevelController::class, 'index']);
 
 //praktikum 5
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//praktimum 6
+Route::get('/user', [UserController::class, 'index']);
