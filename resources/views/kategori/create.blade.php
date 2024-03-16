b93c0b686059319a3914f5e69ab0193280ffb068
Yunila Putmasari <nilaputma26@gmail.com>
2024-03-16 14:29:57 +0700

    Praktikum 3 – Membuat form kemudian menyimpan data dalam database

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -5,18 +5,32 @@
 @section('subtitle', 'kategori')
 @section('content_header_title', 'Kategori')
 @section('content_header_subtitle','Create' )
-{{--Content body: main page content--}}
+
+{{--Content body: main Page content--}}
+
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-header">Manage Kategori</div>
-            <div class="card-body">
-                {{ $dataTable->table() }}
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3>Buat Kategori Baru</h3>
             </div>
-        </div>
+
+            <form method="post" action="../kategori">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="kodeKategori">Kode Kategori</label>
+                <input type="text" class="form-control" id="kodeKategori" name="kodeKategori" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="namaKategori">Nama Kategori</label>
+                <input type="text" class="form-control" id="namaKategori" name="namaKategori" placeholder="">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
     </div>
+</div>      
 @endsection
 
-@push('scripts')
-    {{ $dataTable->scripts() }}
-@endpush
