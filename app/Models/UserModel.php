f87593e9c5ec8dcfb3fc7de024a5bee46c0648a3
Yunila Putmasari <nilaputma26@gmail.com>
2024-04-23 10:41:32 +0700

    JS-10 Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,14 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Contracts\Auth\Authenticatable;
+// use Illuminate\Contracts\Auth\Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model implements Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
 
-    protected $table = "m_users";
-    protected $primaryKey = "user_id";
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    protected $table = 'm_users';
+    protected $primaryKey = 'user_id';
 
     protected $fillable =
         [
@@ -22,53 +32,69 @@ class UserModel extends Model implements Authenticatable
             'nama',
             'password'
         ];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-
-    // public function stok(): HasMany
-    // {
-    //     return $this->hasMany(StokModel::class, 'user_id', 'user_id');
-    // }
-
-    // public function transaksi(): HasMany
-    // {
-    //     return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
-    // }
-
-    // public function updatedTransaksi(): HasMany
-    // {
-    //     return $this->hasMany(PenjualanModel::class, 'updated_by', 'user_id');
-    // }
-    public function getAuthIdentifierName()
-    {
-        return 'user_id';
-    }
-
-    public function getAuthIdentifier()
-    {
-        return $this->user_id;
-    }
-
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    public function getRememberToken()
-    {
-        return $this->remember_token;
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
 }
+
+// class UserModel extends Model implements Authenticatable
+// {
+//     use HasFactory;
+
+//     protected $table = "m_users";
+//     protected $primaryKey = "user_id";
+
+//     protected $fillable =
+//         [
+//             'level_id',
+//             'username',
+//             'nama',
+//             'password'
+//         ];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+
+//     public function stok(): HasMany
+//     {
+//         return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+//     }
+
+//     public function transaksi(): HasMany
+//     {
+//         return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
+//     }
+
+//     public function updatedTransaksi(): HasMany
+//     {
+//         return $this->hasMany(PenjualanModel::class, 'updated_by', 'user_id');
+//     }
+//     public function getAuthIdentifierName()
+//     {
+//         return 'user_id';
+//     }
+
+//     public function getAuthIdentifier()
+//     {
+//         return $this->user_id;
+//     }
+
+//     public function getAuthPassword()
+//     {
+//         return $this->password;
+//     }
+
+//     public function getRememberToken()
+//     {
+//         return $this->remember_token;
+//     }
+
+//     public function setRememberToken($value)
+//     {
+//         $this->remember_token = $value;
+//     }
+
+//     public function getRememberTokenName()
+//     {
+//         return 'remember_token';
+//     }
+// }
